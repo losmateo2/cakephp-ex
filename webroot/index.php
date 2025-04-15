5dd8de2de95159919bff1c9714824f93b451afe5
losmateo2 <19825488+losmateo2@users.noreply.github.com>
2025-04-15 14:40:06 -0500

    Update index.php
    fixed "s i think

--- webroot/index.php
+++ webroot/index.php
@@ -1,6 +1,6 @@
 <?php
   $rand_color = '#' . substr(md5(gethostname()), 0, 6);
   echo "<body style='background-color:$rand_color'>";
-  echo "<h1>hello from " . gethostname()</h1>";
+  echo "<h1>hello from " . gethostname() . "</h1>";
   echo "<br>";
 ?>
