09da1aebb740e70bfcbc24b287c61ff45c555d10
losmateo2 <19825488+losmateo2@users.noreply.github.com>
2025-04-15 14:18:30 -0500

    Update index.php
    using hostname for random number

--- webroot/index.php
+++ webroot/index.php
@@ -1,5 +1,5 @@
 <?php
-  $rand_color = '#' . dechex(mt_rand(0, 16777215));
+  $rand_color = '#' . substr(md5(gethostname()), 0, 6);
   echo "<body style='background-color:$rand_color'>";
   echo "hello from " . gethostname();
   echo "<br>";
